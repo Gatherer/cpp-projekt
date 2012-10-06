0f398debe72c3539bf8cca553a90fe54252657b9
Gatherer <gatherer.thomas@gmx.de>
2012-10-06 15:01:16 +0200

    ADDED initial Tex und Programm Dateien

---
+++ cppProgramm/src/ccpProjekt.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : cppProjekt.cpp
+// Author      : Glatte, Thomas; Gründer Jessica, Dürrschmidt Clemens, Schlabeck Mirco
+// Version     : 0.1
+// Copyright   : Your copyright notice
+// Description : main file for our c++ projekt
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
