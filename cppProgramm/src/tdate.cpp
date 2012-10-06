b300d4b2a5313a3eb788953aec332f6f816bb00e
Clemens Duerrschmidt <clemens.duerrschmidt@googlemail.com>
2012-10-06 18:28:53 +0200

    ADD: cpp- and h-files for first assignment

---
+++ cppProgramm/src/tdate.cpp
@@ -0,0 +1,20 @@
+class TDate
+{
+  private:
+    int Day;
+    int Month;
+    int Year;
+
+  public:
+    void setDate(int Day, int Month, int Year)
+    {
+
+    }
+
+    void print()
+    {
+
+    }
+
+
+}
---
+++ cppProgramm/src/tdate.h
@@ -0,0 +1,11 @@
+class TDate
+{
+  private:
+    int Day;
+    int Month;
+    int Year;
+
+  public:
+    void setDate(int Day, int Month, int Year);
+    void print();
+};
